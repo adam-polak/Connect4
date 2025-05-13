d606e5f4fc019bfa929fae26774c6404b8af50c1
Adam <adamjpolak@gmail.com>
2025-05-13 11:27:14 -0500

    fix errors and start writing tests

--- Backend/internal/game/logic/rules.go
+++ Backend/internal/game/logic/rules.go
@@ -4,12 +4,12 @@ import "connect4/server/internal/game/model"
 
 type board [model.Column][model.Row]uint8
 
-type location struct {
-	column int
-	row    int
+type Location struct {
+	Column int
+	Row    int
 }
 
-type FourInARow [4]location
+type FourInARow [4]Location
 
 func diagonal4InRow(b board) *FourInARow {
 	return nil
@@ -23,7 +23,7 @@ func horizontal4InRow(b board) *FourInARow {
 	return nil
 }
 
-func has4InARow(g model.Game) *FourInARow {
+func Has4InARow(g model.Game) *FourInARow {
 	b := g.GetBoard()
 	diag := diagonal4InRow(b)
 	if diag != nil {
---
+++ Backend/internal/game/logic/rules_test.go
@@ -0,0 +1,56 @@
+package logic
+
+import (
+	"connect4/server/internal/game/model"
+	"testing"
+)
+
+func Test_BasicVertical(t *testing.T) {
+	for c := range model.Column {
+		g := new(model.Game)
+		for range 4 {
+			g.DropPiece(c%2 == 0, c)
+		}
+
+		f := vertical4InRow(g.GetBoard())
+		if f == nil {
+			t.Error("Should detect vertical four in a row")
+			return
+		}
+
+		for r := range 4 {
+			if f[r].Column != c {
+				t.Errorf("Expected column %d was %d", c, f[r].Column)
+			}
+
+			if f[r].Row != r {
+				t.Errorf("Expected row %d was %d", r, f[r].Row)
+			}
+		}
+	}
+}
+
+func Test_BasicHorizontal(t *testing.T) {
+	for i := 0; i+4 < model.Column; i++ {
+		g := new(model.Game)
+		for c := i; c < i+4; c++ {
+			g.DropPiece(i%2 == 0, c)
+		}
+
+		f := horizontal4InRow(g.GetBoard())
+		if f == nil {
+			t.Error("Should detect horizontal four in a row")
+			return
+		}
+
+		for l := range 4 {
+			if f[l].Row != 0 {
+				t.Errorf("Expected row 0 was %d", f[l].Row)
+			}
+
+			if f[l].Column != l+i {
+				t.Errorf("Expected column %d was %d", l+i, f[l].Column)
+			}
+		}
+	}
+}
