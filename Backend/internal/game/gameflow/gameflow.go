6fd70dd0822da172da8ee818f37d6809319ad0d8
Adam <adamjpolak@gmail.com>
2025-05-20 13:42:09 -0500

    starting on player and orchestrator

--- Backend/internal/game/gameflow/gameflow.go
+++ Backend/internal/game/gameflow/gameflow.go
@@ -1,1 +1,8 @@
 package gameflow
+
+import "connect4/server/internal/game/player"
+
+type GameOrchestrator struct {
+	p1 player.Player
+	p2 player.Player
+}
--- Backend/internal/game/player/player.go
+++ Backend/internal/game/player/player.go
@@ -1,17 +1,51 @@
 package player
 
-type Player struct {
+import (
+	"errors"
+	"strings"
+)
+
+type player struct {
 	uid string
+	key string
 }
 
-func NewPlayer(username string, password string) *Player {
-	return &Player{
-		uid: "secret",
-	}
+type playerNode struct {
+	player player
+	left   *playerNode
+	right  *playerNode
 }
 
-func GetPlayer(loginKey string) *Player {
-	return &Player{
-		uid: "secret",
+var players *playerNode = nil
+
+func NewPlayer(loginKey string) {
+	// add player to tree
+}
+
+func getPlayer(p *playerNode, key string) *string {
+	if p == nil {
+		return nil
 	}
+
+	cmp := strings.Compare(p.player.key, key)
+	if cmp < 0 {
+		return getPlayer(p.left, key)
+	} else if cmp > 0 {
+		return getPlayer(p.right, key)
+	}
+
+	return &p.player.uid
+}
+
+func GetPlayer(loginKey string) (string, error) {
+	if players == nil {
+		return "", errors.New("player list must be initialized")
+	}
+
+	uid := getPlayer(players, loginKey)
+	if uid == nil {
+		return "", errors.New("player doesn't exist")
+	}
+
+	return *uid, nil
 }
