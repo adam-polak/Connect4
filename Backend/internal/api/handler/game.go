75147119d2bba932e2161efdbba0f223c6b3e5a8
Adam <adamjpolak@gmail.com>
2025-05-28 13:52:06 -0500

    starting the game handler endpoint

--- Backend/internal/api/handler/game.go
+++ Backend/internal/api/handler/game.go
@@ -1,14 +1,54 @@
 package handler
 
 import (
+	"connect4/server/internal/game/gameflow"
+	"log"
 	"net/http"
 	"strings"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func GameHandler(wr http.ResponseWriter, r *http.Request) {
 	if len(r.Method) != 0 && strings.Compare(r.Method, "GET") != 0 {
-		wr.WriteHeader(400)
+		wr.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
+	key := r.URL.Query().Get("key")
+	if len(key) == 0 {
+		wr.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err := gameflow.GetPlayer(key)
+	if err != nil {
+		wr.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Upgrade HTTP --> WebSocket
+	conn, err := upgrader.Upgrade(wr, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		messageType, buf, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if err := conn.WriteMessage(messageType, buf); err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
